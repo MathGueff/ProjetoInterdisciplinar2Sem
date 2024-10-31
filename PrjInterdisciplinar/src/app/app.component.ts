2b806cc4cf7bff8f79065c8529245d8acc318d3f
Adryann <Adryann2019pontes@gmail.com>
2024-10-30 22:07:31 -0300

    doencas-detalhes adicionado (incompleto)

--- PrjInterdisciplinar/src/app/app.component.ts
+++ PrjInterdisciplinar/src/app/app.component.ts
@@ -5,12 +5,11 @@ import { ReclacacaoPainelComponent } from './reclacacao-painel/reclacacao-painel
 import { MenuUsuarioComponent } from './menu-usuario/menu-usuario.component';
 import { MenuAdminComponent } from './menu-admin/menu-admin.component';
 import { FooterComponent } from './footer/footer.component';
-import { DoencasComponent } from "./doencas/doencas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MenuUsuarioComponent, ReclacacaoPainelComponent, MenuAdminComponent, FooterComponent, DoencasComponent],
+  imports: [RouterOutlet, HeaderComponent, MenuUsuarioComponent, ReclacacaoPainelComponent, MenuAdminComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- PrjInterdisciplinar/src/app/doencas.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Doencas } from './doencas';
-
-describe('Doencas', () => {
-  it('should create an instance', () => {
-    expect(new Doencas()).toBeTruthy();
-  });
-});
