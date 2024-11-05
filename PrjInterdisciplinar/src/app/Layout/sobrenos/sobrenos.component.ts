9b28c9ff0473d36674080dfc734da39f82f20570
Davy Ribeiro <davy.oliv.ribeiro@gmail>
2024-11-04 22:07:43 -0300

    criando o componente e a rota

---
+++ PrjInterdisciplinar/src/app/Layout/sobrenos/sobrenos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobrenos',
+  standalone: true,
+  imports: [],
+  templateUrl: './sobrenos.component.html',
+  styleUrl: './sobrenos.component.css'
+})
+export class SobrenosComponent {
+
+}
--- PrjInterdisciplinar/src/app/app.routes.ts
+++ PrjInterdisciplinar/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { ReclamacaoDescricaoComponent } from './Reclamacao/reclamacao-descricao/reclamacao-descricao.component';
 import { DoencaDetalhesComponent } from './Doenca/doenca-detalhes/doenca-detalhes.component';
 import { DoencasInicialComponent } from './Doenca/doencas-inicial/doencas-inicial.component';
+import { SobrenosComponent } from './Layout/sobrenos/sobrenos.component';
 
 export const routes: Routes = [
   {path: 'doenca-inicial',component:DoencasInicialComponent},
   {path:'pagina-inicial', component:MenuUsuarioComponent},
-  {path: 'reclamacao-descricao',component:ReclamacaoDescricaoComponent}
+  {path: 'reclamacao-descricao',component:ReclamacaoDescricaoComponent},
+  {path: 'sobre-nos', component:SobrenosComponent}
 ];
