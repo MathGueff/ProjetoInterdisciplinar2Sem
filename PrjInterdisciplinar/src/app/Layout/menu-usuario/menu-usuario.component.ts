57d00622d1a5f44dd4c71a1afb25163ac92cdf75
Matheus Augusto Santos Gueff <gueffmatheus@gmail.com>
2024-11-05 21:14:35 -0300

    Adicionado os links do routerLink para o menu de admin

--- PrjInterdisciplinar/src/app/Layout/menu-usuario/menu-usuario.component.ts
+++ PrjInterdisciplinar/src/app/Layout/menu-usuario/menu-usuario.component.ts
@@ -16,6 +16,6 @@ export class MenuUsuarioComponent{
     {path: '/doenca-inicial', nome : 'Doenças', titulo : 'Doenças relacionadas', src : 'assets/icones/icon_black_doenca.svg', info : 'Veja informações sobre as doenças que estão relacionadas a falta de saneamento básico'},
     {path: '/doenca-detalhada', nome : 'Notícias', titulo : 'Notícias sobre saneamento básico', src : 'assets/icones/icon_black_noticia.svg', info : 'Confira algumas das principais notícias sobre saneamento básico e a distribuição de água potável.'},
     {path: '/reclamacao-descricao', nome : 'Reclamações', titulo : 'Faça uma reclamação', src : 'assets/icones/icon_black_reclamacao.svg', info : 'Clique aqui para registrar sua reclamação e ajudar a melhorar a qualidade do abastecimento de água, coleta de esgoto e outros serviços essenciais em sua comunidade!'},
-    {path: '/pagina-inicial', nome : 'Responsáveis', titulo : 'Responsáveis pelo saneamento básico', src : 'assets/icones/icon_black_responsavel.svg', info : 'Identifique os principais responsáveis pelo saneamento básico e descubra como você pode contribuir para melhorar o saneamento em sua cidade.'}
+    {path: '/pagina-admin', nome : 'Responsáveis', titulo : 'Responsáveis pelo saneamento básico', src : 'assets/icones/icon_black_responsavel.svg', info : 'Identifique os principais responsáveis pelo saneamento básico e descubra como você pode contribuir para melhorar o saneamento em sua cidade.'}
   ];
 }
