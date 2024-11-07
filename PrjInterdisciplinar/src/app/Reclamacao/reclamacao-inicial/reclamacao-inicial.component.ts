fced5193df577946e630b7f2fa877068bcb1a8e4
devDavyRibeiro <you@example.comdavy.oliv.ribeiro@gmail.com>
2024-11-07 18:17:10 -0300

    criação da reclamação inicial

---
+++ PrjInterdisciplinar/src/app/Reclamacao/reclamacao-inicial/reclamacao-inicial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reclamacao-inicial',
+  standalone: true,
+  imports: [],
+  templateUrl: './reclamacao-inicial.component.html',
+  styleUrl: './reclamacao-inicial.component.css'
+})
+export class ReclamacaoInicialComponent {
+
+}
--- PrjInterdisciplinar/src/app/app.routes.ts
+++ PrjInterdisciplinar/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { Routes } from '@angular/router';
 import { ReclamacaoDescricaoComponent } from './Reclamacao/reclamacao-descricao/reclamacao-descricao.component';
 import { DoencaDetalhesComponent } from './Doenca/doenca-detalhes/doenca-detalhes.component';
 import { DoencasInicialComponent } from './Doenca/doencas-inicial/doencas-inicial.component';
+import { MenuAdminComponent } from './Layout/menu-admin/menu-admin.component';
+import { ReclamacaoInicialComponent } from './Reclamacao/reclamacao-inicial/reclamacao-inicial.component';
 
 export const routes: Routes = [
   {path:'pagina-inicial', component:MenuUsuarioComponent},
   {path: 'pagina-admin', component:MenuAdminComponent},
   {path: 'doenca-inicial',component:DoencasInicialComponent},
   {path:'pagina-inicial', component:MenuUsuarioComponent},
-  {path: 'reclamacao-descricao',component:ReclamacaoDescricaoComponent}
+  {path: 'reclamacao-descricao',component:ReclamacaoDescricaoComponent},
+  {path:'reclamacao-inicial', component:ReclamacaoInicialComponent}
 ];
