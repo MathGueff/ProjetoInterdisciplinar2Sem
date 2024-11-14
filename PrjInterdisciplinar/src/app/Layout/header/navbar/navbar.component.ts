d693b9841bce91b2a80c076cf16239793d5bf910
devDavyRibeiro <you@example.comdavy.oliv.ribeiro@gmail.com>
2024-11-14 16:52:33 -0300

    Finalizando reclamacao inicial com a implementação da reclamação descrição

--- PrjInterdisciplinar/src/app/Layout/header/navbar/navbar.component.ts
+++ PrjInterdisciplinar/src/app/Layout/header/navbar/navbar.component.ts
@@ -17,7 +17,6 @@ export class NavbarComponent {
     {path: '/doenca-inicial', img: 'assets/icones/icon_white_doenca.svg', alt : 'Ícone do link para página de Doenças', nome : 'Doenças'},
     {path: '/doenca-detalhada', img: 'assets/icones/icon_white_noticia.svg', alt : 'Ícone do link para página de Notícias', nome : 'Notícias'},
     {path: '/reclamacao-inicial', img: 'assets/icones/icon_white_reclamacao.svg', alt : 'Ícone do link para página de Reclamação', nome : 'Reclamação'},
-    {path: '/reclamacao-descricao', img: 'assets/icones/icon_white_reclamacao.svg', alt : 'Ícone do link para página de Reclamação', nome : 'Reclamação'},
     {path: '/pagina-admin', img: 'assets/icones/icon_white_responsavel.svg', alt : 'Ícone do link para página de Reclamação', nome : 'Admin'}
   ]
 }
