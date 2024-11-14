436a59ebd10eea68b84d310377feec059aaa5156
Davy Ribeiro <davy.oliv.ribeiro@gmail>
2024-11-13 21:48:13 -0300

    atualizações

--- PrjInterdisciplinar/src/app/app.routes.ts
+++ PrjInterdisciplinar/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { ReclamacaoDescricaoComponent } from './Reclamacao/reclamacao-descricao/
 import { DoencaDetalhesComponent } from './Doenca/doenca-detalhes/doenca-detalhes.component';
 import { DoencasInicialComponent } from './Doenca/doencas-inicial/doencas-inicial.component';
 import { SobrenosComponent } from './Layout/sobrenos/sobrenos.component';
-import { MenuAdminComponent } from './Layout/menu-admin/menu-admin.component';
+import { MenuAdminComponent } from './Admin/menu-admin/menu-admin.component';
 import { FormLoginComponent } from './Login/form-login/form-login.component';
 import { FormCadastroComponent } from './Login/form-cadastro/form-cadastro.component';
 import { ReclamacaoInicialComponent } from './Reclamacao/reclamacao-inicial/reclamacao-inicial.component';
