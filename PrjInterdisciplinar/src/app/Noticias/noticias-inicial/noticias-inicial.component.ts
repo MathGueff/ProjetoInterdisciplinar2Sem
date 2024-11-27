5f50c41ec1ce0431d62dceea38f28b702b73c165
AdryannDev <adryann2019pontes@gmail.com>
2024-11-26 21:54:07 -0300

    continuação

--- PrjInterdisciplinar/src/app/Noticias/noticias-inicial/noticias-inicial.component.ts
+++ PrjInterdisciplinar/src/app/Noticias/noticias-inicial/noticias-inicial.component.ts
@@ -1,7 +1,10 @@
+import { Noticia } from './../../models/noticia';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Noticia } from '../../models/noticia';
 import { NoticiasCardComponent } from '../noticias-card/noticias-card.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-noticias-inicial',
@@ -11,6 +14,9 @@ import { NoticiasCardComponent } from '../noticias-card/noticias-card.component'
   styleUrl: './noticias-inicial.component.css'
 })
 export class NoticiasInicialComponent {
+  private NoticiaSubject =new BehaviorSubject<Noticia[]>([] as any);
+  data$:Observable<Noticia[]> = this.NoticiaSubject.asObservable();
+  TagSelect: FormGroup;
   noticias: Noticia[] = [
     {
       id: 1,
@@ -18,6 +24,7 @@ export class NoticiasInicialComponent {
       description:
         'Prefeitos eleitos vão governar em ciclo decisivo para metas do saneamento',
       image: 'img/paginas/noticias/noticia1.webp',
+
     },
     {
       id: 2,
@@ -33,5 +40,31 @@ export class NoticiasInicialComponent {
         'Prefeitos eleitos vão governar em ciclo decisivo para metas do saneamento',
       image: 'img/paginas/noticias/noticia1.webp',
     }
-  ]
+  ];
+
+  constructor(private fb:FormBuilder){
+    this.NoticiaSubject.next(this.noticias);
+    this.TagSelect = this.fb.group({
+        tagForm: ['Todos']
+      }
+    );
+   }
+
+   ngOnInit():void{
+    this.TagSelect.valueChanges.subscribe(() => {
+      let lista : Noticia[] = [];
+      //Verifica se nenhuma Tag foi selecionada
+      if(this.TagSelect.value.tagForm === "Todas"){
+        lista = this.noticias;
+      }
+      // Filtra o array de Reclamações pela tag selecionada
+      else{
+        lista = this.noticias.filter((Noticia) =>{
+        return this.noticias.tags === this.TagSelect.value.tagForm
+      });
+      }
+      //atualizando o valor do Observabale
+      this.doencaSubject.next(lista);
+    })
+  }
 }
